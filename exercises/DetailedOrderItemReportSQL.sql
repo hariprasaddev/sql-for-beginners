8c66dce226fc654bb65fd67c00f3eff2d75b6fd6
Hariprasad18 <mukkerahariprasad1849@gmail.com>
2026-05-23 15:20:08 +0530

    ✅ Add solution for: Detailed Order Item Report - SQL

---
+++ exercises/DetailedOrderItemReportSQL.sql
@@ -0,0 +1,12 @@
+-- Write a SQL query below --
+SELECT
+oi.order_id,
+p.name AS product_name,
+oi.quantity,
+ROUND(oi.price,2) AS price
+FROM order_items oi
+INNER JOIN products p
+ON oi.product_id = p.id
+WHERE oi.quantity > 1
+ORDER BY oi.order_id ASC;
+
